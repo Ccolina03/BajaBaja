4dc82ffdf1cbc33d7fb76d4277c22d1630f97059
Ccolina03 <colinaperu2003@gmail.com>
2022-10-25 04:34:43 -0600

    Updates

---
+++ precache-manifest.4003be13d5e4d334532820cc8b9261fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f33624a5a91f0b6664d15b4b8fb33ea",
+    "url": "/BajaBaja/index.html"
+  },
+  {
+    "revision": "d269615d62eb2a23f60a",
+    "url": "/BajaBaja/static/css/main.0a7cefd6.chunk.css"
+  },
+  {
+    "revision": "b8b809be9ddd11b39c3a",
+    "url": "/BajaBaja/static/js/2.4654ef22.chunk.js"
+  },
+  {
+    "revision": "d269615d62eb2a23f60a",
+    "url": "/BajaBaja/static/js/main.dd23a69e.chunk.js"
+  },
+  {
+    "revision": "b6bd9a0ac7bed47a2a88",
+    "url": "/BajaBaja/static/js/runtime-main.e159f5d3.js"
+  }
+]);
