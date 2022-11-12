03693acb3d016cb0a1a153b79b4735aeb1e0400b
Ccolina03 <colinaperu2003@gmail.com>
2022-11-11 23:20:30 -0700

    Reading Week advancement

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './NewPlace.css';
+import Input from '../../shared/components/FormElements/Input';
+
+import { VALIDATOR_REQUIRE } from  '../../shared/components/util/validators';
 
 const NewPlace = () => {
 	return <form className="place-form">
-		
-
+		<Input element = "input" type ="text" label ="Title" 
+		validators={[VALIDATOR_REQUIRE]}
+		errorText="Please enter a valid bus stop name"/>
 	</form>;
-}
+};
 
 export default NewPlace;
