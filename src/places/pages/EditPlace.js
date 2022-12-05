96938542ff62af73bc9e8dbb834cc933f9c28513
Carlos Colina <107608945+Ccolina03@users.noreply.github.com>
2022-12-04 22:54:22 -0700

    Adding Edit Bus Stop functionality

--- src/places/pages/EditPlace.js
+++ src/places/pages/EditPlace.js
@@ -1,44 +1,27 @@
 import React from 'react';
-import { useEffect } from 'react';
-import {useParams} from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
+import {useParams, useHistory} from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
-import './NewPlace.css';
+import './NewPlace.css'; //same for CSS stye for this update form to the New Bus Stop file
 
 import Card from '../../shared/components/UIElements/Card';
 import {useForm } from '../../shared/hooks/form-hook';
-const INITIAL_DATA=[ 
-    {
-        id: "p1",
-        title: "Samoa Stop",
-        description: 'This stop is in front of a kindergarten. There is stop light to cross the street, but no proper cover from the rain.',
-        imageUrl: "https://as2.ftcdn.net/v2/jpg/02/04/43/63/1000_F_204436318_P6lTrj1G3UnpGIP9C7JfN2WuQlBclbA0.jpg",
-        address: 'Interseccion Av. La Molina con calle Samoa',
-        location: {
-            lat: -12.086158,
-            lng:  -76.898019
-        },
-        creator: "u1"
-    },
-    
-    {
-        id: "p2",
-        title: "Universidad Agraria Puerta 4 Stop",
-        description: 'My first ever bus stop. In front of "rejas" and next to the soccer fields. Proper infrastructure for a bus stop.',
-        imageUrl: "https://gestion.pe/resizer/ShN6wyx2fmgzbBW8d7nbNRrCZFk=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/ACJCPGAFKBDSBEDR2SZADWQRPI.jpeg",
-        address: 'Av. La Molina Puerta 4',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: "u2"
-    }
-    
-    ]
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import { AuthContext} from '../../shared/context/auth-context';
+
 
  const EditPlace = () => {
+
+    const {isLoading, error, sendRequest, clearError} = useHttpClient();
+    const [loadedPlaces, setLoadedPlaces] = useState()
+    
+    const auth = useContext(AuthContext);
     const placeId= useParams().placeId;
+    const history = useHistory();
 
     const [formState, inputHandler, setFormData] = useForm({
         title: {
@@ -51,31 +34,66 @@ const INITIAL_DATA=[
         }
     }, false)
 
-    //backend part to be replaced
-    const identifiedPlace = INITIAL_DATA.find(p => p.id === placeId);  
+    
+  useEffect(() => {
+    const fetchPlace = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/${placeId}`
+        );
+        setLoadedPlaces(responseData.bus_stop);
+        setFormData(
+          {
+            title: {
+              value: responseData.bus_stop.title,
+              isValid: true
+            },
+            description: {
+              value: responseData.bus_stop.description,
+              isValid: true
+            }
+          },
+          true
+        );
+      
+      } catch (err) {}
+    };
+    fetchPlace();
+  }, [sendRequest, placeId, setFormData]);
 
-
-    useEffect(() => {
-    if (identifiedPlace) {  setFormData({ title: {
-        value: identifiedPlace.title,
-        isValid: true
-    },
-    description: {
-        value: identifiedPlace.description,
-        isValid: true
-    }
-        
-    }, true);
-
-}  } ,[identifiedPlace, setFormData]);
+   
     
 
-    const placeUpdateSubmitHandler = event => {
+    const placeUpdateSubmitHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs);
-    }
+        try {
+          await sendRequest(`http://localhost:5000/api/places/${placeId}`, 'PATCH', 
+        JSON.stringify({
+          title: formState.inputs.title.value, 
+          description: formState.inputs.description.value
+        }),
+        {
+          'Content-Type': 'application/json'
+        }
+        );
+        history.push('/' + auth.userId +'/places');
+        } catch (err) {
 
-    if (!identifiedPlace) {
+        }
+
+    };
+
+
+    if (isLoading) {
+        return (
+            <div className='center'>
+               <LoadingSpinner/>
+            </div>
+        );
+     }
+    
+
+    if (!loadedPlaces && !error ) {
     return (
       <div className="center">
         <Card>
@@ -86,16 +104,12 @@ const INITIAL_DATA=[
      }
 
      
-     if (!formState.inputs.title.value) {
-        return (
-            <div className='center'>
-                <h2> Still loading. Give me a second :/ </h2>
-            </div>
-        );
-     }
-    
+
      return (
-       formState.inputs.title.value && <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+        <React.Fragment>
+        <ErrorModal error={error} onClear={clearError} />
+       {!isLoading && loadedPlaces && (  
+       <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
           <Input
             id="title"
             element="input"
@@ -104,8 +118,8 @@ const INITIAL_DATA=[
             validators={[VALIDATOR_REQUIRE()]}
             errorText="The title is not valid. Try again."
             onInput={inputHandler}
-            initialValue={formState.inputs.title.value}
-            initialValid={formState.inputs.title.isValid}
+            initialValue={loadedPlaces.title}
+            initialValid={true}
           />
           <Input
             id="description"
@@ -114,13 +128,14 @@ const INITIAL_DATA=[
             validators={[VALIDATOR_MINLENGTH(5)]}
             errorText="The minimum amount of characters allowed: 5. Please try again. "
             onInput={inputHandler}
-            initialValue={formState.inputs.description.value}
-            initialValid={formState.inputs.description.isValid}
-          />
+            initialValue={loadedPlaces.description}
+            initialValid={true}
+          />E
           <Button type="submit" disabled={!formState.isValid}>
             Edit Bus Stop
           </Button>
-        </form>
+        </form>)}
+        </React.Fragment>
       );
     };
     
